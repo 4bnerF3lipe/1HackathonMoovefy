deff538166b74a4b480f910a99c0b0f55b639802
GAMASTI\ian.malavazi <ian.malavazi@gamasti.com.br>
2023-04-29 16:46:30 -0300

    Criação do projeto Web

---
+++ hackathon_web/PetLoc/src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<script>
+import { defineComponent } from "vue";
+const name = 'HomeView'
+
+export default defineComponent({
+  name,
+
+  components: { }, 
+
+  props: { },
+
+  mounted () { },
+
+  updated () { },
+
+  data () {
+    return { }
+  },
+
+  methods: { },
+
+  computed: { }, 
+
+});
+</script>
+
+<template>
+    <div>a</div>
+</template>
+
+
+<style scoped>
+
+</style>
