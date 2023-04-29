0293eddc20ab5a267d7b9bd5bd3749874edd9bad
ianer-glitch <ianrepker@gmail.com>
2023-04-29 18:19:15 -0300

    no message

---
+++ hackathon_web/PetLoc/src/components/atoms/TituloPrimaio.vue
@@ -0,0 +1,47 @@
+<script>
+import { defineComponent } from "vue";
+const name = 'TituloPrimaio'
+
+export default defineComponent({
+  name,
+
+  components: { }, 
+
+  props: { 
+    texto:{
+        type:String,
+        default:"TITULO"
+    },
+    corTexto:{
+        type:String,
+        default:"#707070"
+    }
+    
+  },
+
+  mounted () { },
+
+  updated () { },
+
+  data () {
+    return { }
+  },
+
+  methods: { },
+
+  computed: { }, 
+
+});
+</script>
+
+<template>
+    <p class = "texto">{{ texto }}</p>
+</template>
+
+
+<style scoped>
+.texto{
+    color:v-bind('corTexto');
+    font-size: 3rem;
+}
+</style>
---
+++ hackathon_web/PetLoc/src/components/molecules/PrimeiraSessaoTextoBase.vue
@@ -0,0 +1,57 @@
+<script>
+import { defineComponent } from "vue";
+import SubtituloPrimario from "../atoms/SubtituloPrimario.vue";
+import TituloPrimaio from "../atoms/TituloPrimaio.vue";
+const name = 'PrimeiraSessaoTextoBase'
+
+export default defineComponent({
+  name,
+
+  components: {TituloPrimaio, SubtituloPrimario }, 
+
+  props: { },
+
+  mounted () { },
+
+  updated () { },
+
+  data () {
+    return { }
+  },
+
+  methods: { },
+
+  computed: { }, 
+
+});
+</script>
+
+<template>
+    <div class = "content">
+        <div class="conteudo-primario">
+            <titulo-primaio/>
+            <subtitulo-primario/>
+        </div>
+        <div class="conteudo-secundario">
+            <titulo-primaio />
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+
+.content,.conteudo-primario{
+    width:100%;
+    height: 100%;
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+}
+.content{
+    gap:10px;
+}
+.conteudo-primario{
+    margin-top: 10px;;
+}
+</style>
