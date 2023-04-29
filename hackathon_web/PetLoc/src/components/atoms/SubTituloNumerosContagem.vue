a1d29cd2de3dee23d6aab08e49ea24d9e74458dc
GAMASTI\natalia.mangili <natalia.mangili@gamasti.com.br>
2023-04-29 18:22:06 -0300

    Contadores

---
+++ hackathon_web/PetLoc/src/components/atoms/SubTituloNumerosContagem.vue
@@ -0,0 +1,46 @@
+<script>
+import { defineComponent } from "vue";
+const name = 'SubTituloNumerosContagem'
+
+export default defineComponent({
+  name,
+
+  components: { }, 
+
+  props: {
+    subtitulo:{
+        type: String,
+        default: "N/A",
+    },
+    corsubtitulo:{
+        type: String,
+        default: "#707070",
+    }
+   },
+
+  mounted () { },
+
+  updated () { },
+
+  data () {
+    return { }
+  },
+
+  methods: { },
+
+  computed: { }, 
+
+});
+</script>
+
+<template>
+    <div class="subtitulo"> {{subtitulo}}</div>
+</template>
+
+
+<style scoped>
+.subtitulo{
+    text-align: center;
+    color: v-bind('corsubtitulo');
+}
+</style>
---
+++ hackathon_web/PetLoc/src/components/molecules/ComponenteTotalizador.vue
@@ -0,0 +1,39 @@
+<script>
+import { defineComponent } from "vue";
+import NumerosContagem from "../atoms/NumerosContagem.vue";
+import SubTituloNumerosContagem from "../atoms/SubTituloNumerosContagem.vue";
+const name = 'ComponenteTotalizador'
+
+export default defineComponent({
+  name,
+
+  components: { NumerosContagem, SubTituloNumerosContagem }, 
+
+  props: { },
+
+  mounted () { },
+
+  updated () { },
+
+  data () {
+    return { }
+  },
+
+  methods: { },
+
+  computed: { }, 
+
+});
+</script>
+
+<template>
+    <div>
+    <NumerosContagem numero="123125"/>
+    <SubTituloNumerosContagem subtitulo="pets"/>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
